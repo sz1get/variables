1f78bc13be6905315f9ed40855a5efb1250dbc21
sz1get <sziget4283@gmail.com>
2024-10-08 17:24:38 +0300

    Add files via upload

---
+++ varyable.py
@@ -0,0 +1,8 @@
+first_name='number of completed homeworks'
+homeworks_count=12
+second_name='number of hours spent'
+hours_count=1.5
+name_of_course='Python'
+third_name='time_for_one_task'
+time=1.5/12
+print('Course:',name_of_course,',',first_name,':',homeworks_count,',',second_name,':',hours_count,',',third_name,':',time)
